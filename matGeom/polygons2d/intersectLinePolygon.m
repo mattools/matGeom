a15d79b9dc2836eaa498ce71ce72bf36bebf8f3d
David Legland <david.legland@nantes.inra.fr>
2011-07-20 08:25:27 +0000

    polygons/intersectLinePolygon.m: add possibility to return edge indices

--- matGeom/polygons2d/intersectLinePolygon.m
+++ matGeom/polygons2d/intersectLinePolygon.m
@@ -1,33 +1,52 @@
-function intersects = intersectLinePolygon(line, poly, varargin)
+function [intersects edgeIndices] = intersectLinePolygon(line, poly, varargin)
 %INTERSECTLINEPOLYGON Intersection points between a line and a polygon
 %
 %   P = intersectLinePolygon(LINE, POLY)
 %   Returns the intersection points of the lines LINE with polygon POLY. 
-%   LINE is a 1x4 array containing parametric representation of the line
-%   (in the form [x0 y0 dx dy], see createLine for details). 
-%   POLY is a NVx2 array containing coordinates of the NV polygon vertices
-%   P is a NIx2 array containing the coordinates of the M intersection
+%   LINE is a 1-by-4 row vector containing parametric representation of the
+%   line (in the format [x0 y0 dx dy], see the function 'createLine' for
+%   details). 
+%   POLY is a NV-by-2 array containing coordinates of the polygon vertices
+%   P is a K-by-2 array containing the coordinates of the K intersection
 %   points.
 %
 %   P = intersectLinePolygon(LINE, POLY, TOL)
 %   Specifies the tolerance for geometric tests. Default is 1e-14.
 %
-%   Example
+%   [P INDS] = intersectLinePolygon(...)
+%   Also returns the indices of edges involved in intersections. INDS is a
+%   K-by-1 column vector, such that P(i,:) corresponds to intersection of
+%   the line with the i-th edge of the polygon. If the intersection occurs
+%   at a polygon vertex, the index of only one of the two neighbor edges is
+%   returned. 
+%   Note that due to numerical approximations, the use of function
+%   'isPointOnEdge' may give results not consistent with this function.
+%
+%
+%   Examples
 %   % compute intersections between a square and an horizontal line
-%   poly = [0 0;10 0;10 10;0 10];
-%   line = [5 5 1 0];
-%   intersectLinePolygon(line, poly)
-%   ans =
-%         10     5
-%          0     5
+%     poly = [0 0;10 0;10 10;0 10];
+%     line = [5 5 1 0];
+%     intersectLinePolygon(line, poly)
+%     ans =
+%           10     5
+%            0     5
+%     % also return indices of edges
+%     [inters inds] = intersectLinePolygon(line, poly)
+%     inters =
+%           10     5
+%            0     5
+%     inds =
+%           4
+%           2
 %      
 %   % compute intersections between a square and a diagonal line
-%   poly = [0 0;10 0;10 10;0 10];
-%   line = [5 5 1 1];
-%   intersectLinePolygon(line, poly)
-%   ans =
-%          0     0
-%         10    10
+%     poly = [0 0;10 0;10 10;0 10];
+%     line = [5 5 1 1];
+%     intersectLinePolygon(line, poly)
+%     ans =
+%            0     0
+%           10    10
 %
 %   See Also
 %   lines2d, polygons2d, intersectLines, intersectRayPolygon
@@ -40,11 +59,13 @@ function intersects = intersectLinePolygon(line, poly, varargin)
 
 %   HISTORY
 %   2008-11-24 rename 'pi' as 'intersects', update doc
-%   2009-23-07 removed forgotten occurence of 'pi' variable (thanks to Bala
+%   2009-07-23 removed forgotten occurence of 'pi' variable (thanks to Bala
 %       Krishnamoorthy)
 %   2010-01-26 rewrite using vectorisation
 %   2011-05-20 returns unique results
+%   2011-07-20 returns intersected edge indices
 
+% get computation tolerance
 tol = 1e-14;
 if ~isempty(varargin)
     tol = varargin{1};
@@ -58,13 +79,24 @@ edges = [poly(1:N, :) poly([2:N 1], :)];
 supportLines = edgeToLine(edges);
 intersects = intersectLines(line, supportLines, tol);
 
-% compute position of intersection points on corresponding lines,
-% and keep only intersection points located on edges
-inds = find(isfinite(intersects(:,1)));
+% find edges that are not parallel to the input line
+inds = find(isfinite(intersects(:, 1)));
+
+% compute position of intersection points on corresponding lines
 pos = linePosition(intersects(inds, :), supportLines(inds, :));
-b = pos > -tol & pos-1 < tol;
-intersects = intersects(inds(b), :);
+
+% and keep only intersection points located on edges
+b = pos > -tol & pos < 1+tol;
+inds = inds(b);
+intersects = intersects(inds, :);
 
 % remove multiple vertices (can occur for intersections located at polygon
 % vertices)
-intersects = unique(intersects, 'rows');
+[intersects I J] = unique(intersects, 'rows'); %#ok<NASGU>
+
+if nargout > 1
+    % return indices of edges involved in intersection
+    % (in case of intersection located at a vertex, only one of the
+    % neighbor edges is returned)
+    edgeIndices = inds(I);
+end
--- tests/polygons2d/testIntersectLinePolygon.m
+++ tests/polygons2d/testIntersectLinePolygon.m
@@ -1,9 +1,9 @@
 function test_suite = testIntersectLinePolygon(varargin) %#ok<STOUT>
-%TESTINTERSECTLINES  One-line description here, please.
-%   output = testIntersectLines(input)
+%TESTINTERSECTLINEPOLYGON Test case for function intersectLinePolygon
+%   output = testIntersectLinePolygon(input)
 %
 %   Example
-%   testIntersectLines
+%   testIntersectLinePolygon
 %
 %   See also
 %
@@ -13,13 +13,32 @@ function test_suite = testIntersectLinePolygon(varargin) %#ok<STOUT>
 % e-mail: david.legland@grignon.inra.fr
 % Created: 2009-04-22,    using Matlab 7.7.0.471 (R2008b)
 % Copyright 2009 INRA - Cepia Software Platform.
-% Licensed under the terms of the LGPL, see the file "license.txt"
 
 initTestSuite;
 
 
 function testSquare %#ok<*DEFNU>
+% test with a square and orthogonal lines, inside and outside
+
 poly = [0 0;10 0;10 10;0 10];
+
+lineH1 = [5 5 1 0];
+assertEqual(2, size(intersectLinePolygon(lineH1, poly), 1));
+
+lineH2 = [5 15 1 0];
+assertEqual(0, size(intersectLinePolygon(lineH2, poly), 1));
+
+lineV1 = [5 5 0 1];
+assertEqual(2, size(intersectLinePolygon(lineV1, poly), 1));
+
+lineV2 = [15 5 0 1];
+assertEqual(0, size(intersectLinePolygon(lineV2, poly), 1));
+
+
+function testClosedSquare
+% test when the polygon has same vertices at end and at bginning
+
+poly = [0 0;10 0;10 10;0 10;0 0];
 
 lineH1 = [5 5 1 0];
 assertEqual(2, size(intersectLinePolygon(lineH1, poly), 1));
@@ -42,6 +61,10 @@ target = [10 0];
 intersects = intersectLinePolygon(lineH0, poly);
 assertTrue(ismember(target, intersects, 'rows'));
 
+[intersects inds] = intersectLinePolygon(lineH0, poly);
+assertEqual(size(intersects, 1), size(inds, 1));
+
+
 lineH1 = [10 10 3 0];
 intersects = intersectLinePolygon(lineH1, poly);
 target = [20 10];
@@ -49,15 +72,27 @@ assertTrue(ismember(target, intersects, 'rows'));
 target = [0 10];
 assertTrue(ismember(target, intersects, 'rows'));
 
+[intersects inds] = intersectLinePolygon(lineH1, poly);
+assertEqual(size(intersects, 1), size(inds, 1));
+
+
 lineH2 = [0 20 3 0];
 intersects = intersectLinePolygon(lineH2, poly);
 target = [10 20];
 assertTrue(ismember(target, intersects, 'rows'));
 
+[intersects inds] = intersectLinePolygon(lineH2, poly);
+assertEqual(size(intersects, 1), size(inds, 1));
+
+
 lineV0 = [0 0 0 3];
 target = [0 10];
 intersects = intersectLinePolygon(lineV0, poly);
 assertTrue(ismember(target, intersects, 'rows'));
+
+[intersects inds] = intersectLinePolygon(lineV0, poly);
+assertEqual(size(intersects, 1), size(inds, 1));
+
 
 lineV1 = [10 10 0 3];
 intersects = intersectLinePolygon(lineV1, poly);
@@ -66,10 +101,30 @@ assertTrue(ismember(target, intersects, 'rows'));
 target = [10 0];
 assertTrue(ismember(target, intersects, 'rows'));
 
+[intersects inds] = intersectLinePolygon(lineV1, poly);
+assertEqual(size(intersects, 1), size(inds, 1));
+
+
 lineV2 = [20 0 0 3];
 intersects = intersectLinePolygon(lineV2, poly);
 target = [20 10];
 assertTrue(ismember(target, intersects, 'rows'));
+
+[intersects inds] = intersectLinePolygon(lineV2, poly);
+assertEqual(size(intersects, 1), size(inds, 1));
+
+
+function testMShape
+% a more complicated polygon, with 4 intersections
+
+poly = [10 10;60 10;60 40;40 20;30 20;10 40];
+line = [0 30 3 0];
+
+[inters inds] = intersectLinePolygon(line, poly);
+expInters = [10 30;20 30;50 30;60 30];
+assertEqual(expInters, inters);
+expInds = [6;5;3;2];
+assertEqual(expInds, inds);
 
 
 function testUniquePoints
@@ -79,3 +134,13 @@ poly = [0 0;10 0;10 10;0 10];
 line = [5 5 1 1];
 intersects = intersectLinePolygon(line, poly);
 assertEqual(2, size(intersects, 1), 'Wrong number of intersections');
+
+function testGetEdgesIndices
+
+poly = [0 0;10 0;10 10;0 10];
+line = [5 5 1 0];
+[intersects inds] = intersectLinePolygon(line, poly); %#ok<ASGLU>
+
+assertEqual(2, length(inds));
+assertEqual([4;2], inds);
+
