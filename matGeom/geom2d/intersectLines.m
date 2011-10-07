9e6ee31cdc8274b45c26c1ba48683277da039891
David Legland <david.legland@nantes.inra.fr>
2011-10-07 14:48:44 +0000

    geom2d: cleanup code for itnersectLines

--- matGeom/geom2d/intersectLines.m
+++ matGeom/geom2d/intersectLines.m
@@ -2,9 +2,9 @@ function point = intersectLines(line1, line2, varargin)
 %INTERSECTLINES Return all intersection points of N lines in 2D
 %
 %   PT = intersectLines(L1, L2);
-%   returns the intersection point of lines L1 and L2. L1 and L2 are [1*4]
-%   arrays, containing parametric representation of each line (in the form
-%   [x0 y0 dx dy], see 'createLine' for details).
+%   returns the intersection point of lines L1 and L2. L1 and L2 are 1-by-4
+%   row arrays, containing parametric representation of each line (in the
+%   form [x0 y0 dx dy], see 'createLine' for details).
 %   
 %   In case of colinear lines, returns [Inf Inf].
 %   In case of parallel but not colinear lines, returns [NaN NaN].
@@ -35,67 +35,92 @@ function point = intersectLines(line1, line2, varargin)
 %
 
 %   HISTORY
-%   19/02/2004 add support for multiple lines.
-%   08/03/2007 update doc
+%   2004-02-19 add support for multiple lines.
+%   2007-03-08 update doc
+%   2011-10-07 code cleanup
 
-% extreact tolerance
+
+%% Process input arguments
+
+% extract tolerance
 tol = 1e-14;
 if ~isempty(varargin)
     tol = varargin{1};
 end
 
-x1 =  line1(:,1);
-y1 =  line1(:,2);
-dx1 = line1(:,3);
-dy1 = line1(:,4);
+% check size of each input
+N1 = size(line1, 1);
+N2 = size(line2, 1);
+N = max(N1, N2);
+if N1 ~= N2 && N1*N2 ~= N
+    error('matGeom:IntersectLines:IllegalArgument', ...
+        'The two input arguments must have same number of lines');
+end
 
-x2 =  line2(:,1);
-y2 =  line2(:,2);
-dx2 = line2(:,3);
-dy2 = line2(:,4);
 
-N1 = length(x1);
-N2 = length(x2);
+%% Check parallel and colinear lines
+
+% coordinate differences of origin points
+dx = bsxfun(@minus, line2(:,1), line1(:,1));
+dy = bsxfun(@minus, line2(:,2), line1(:,2));
 
 % indices of parallel lines
-par = abs(dx1.*dy2 - dx2.*dy1) < tol;
+denom = line1(:,3) .* line2(:,4) - line2(:,3) .* line1(:,4);
+par = abs(denom) < tol;
 
 % indices of colinear lines
-col = abs((x2-x1) .* dy1 - (y2-y1) .* dx1) < tol & par ;
+col = abs(dx .* line1(:,4) - dy .* line1(:,3)) < tol & par ;
 
+% initialize result array
+x0 = zeros(N, 1);
+y0 = zeros(N, 1);
+
+% initialize result for parallel lines
 x0(col) = Inf;
 y0(col) = Inf;
 x0(par & ~col) = NaN;
 y0(par & ~col) = NaN;
 
-i = ~par;
-
-% compute intersection points
-if N1==N2
-	x0(i) = ((y2(i)-y1(i)).*dx1(i).*dx2(i) + x1(i).*dy1(i).*dx2(i) - x2(i).*dy2(i).*dx1(i)) ./ ...
-        (dx2(i).*dy1(i)-dx1(i).*dy2(i)) ;
-	y0(i) = ((x2(i)-x1(i)).*dy1(i).*dy2(i) + y1(i).*dx1(i).*dy2(i) - y2(i).*dx2(i).*dy1(i)) ./ ...
-        (dx1(i).*dy2(i)-dx2(i).*dy1(i)) ;
-    
-elseif N1==1
-	x0(i) = ((y2(i)-y1).*dx1.*dx2(i) + x1.*dy1.*dx2(i) - x2(i).*dy2(i).*dx1) ./ ...
-        (dx2(i).*dy1-dx1.*dy2(i)) ;
-	y0(i) = ((x2(i)-x1).*dy1.*dy2(i) + y1.*dx1.*dy2(i) - y2(i).*dx2(i).*dy1) ./ ...
-        (dx1.*dy2(i)-dx2(i).*dy1) ;
-    
-elseif N2==1
-   	x0(i) = ((y2-y1(i)).*dx1(i).*dx2 + x1(i).*dy1(i).*dx2 - x2.*dy2.*dx1(i)) ./ ...
-        (dx2.*dy1(i)-dx1(i).*dy2) ;
-	y0(i) = ((x2-x1(i)).*dy1(i).*dy2 + y1(i).*dx1(i).*dy2 - y2.*dx2.*dy1(i)) ./ ...
-        (dx1(i).*dy2-dx2.*dy1(i)) ;
-    
-else
-    % formattage a rajouter
-   	x0(i) = ((y2(i)-y1(i)).*dx1(i).*dx2(i) + x1(i).*dy1(i).*dx2(i) - x2(i).*dy2(i).*dx1(i)) ./ ...
-        (dx2(i).*dy1(i)-dx1(i).*dy2(i)) ;
-	y0(i) = ((x2(i)-x1(i)).*dy1(i).*dy2(i) + y1(i).*dx1(i).*dy2(i) - y2(i).*dx2(i).*dy1(i)) ./ ...
-        (dx1(i).*dy2(i)-dx2(i).*dy1(i)) ;
+% in case all line couples are parallel, return
+if all(par)
+    point = [x0 y0];
+    return;
 end
 
+
+%% Extract coordinates of itnersecting lines
+
+% indices of intersecting lines
+inds = ~par;
+
+% extract base coordinates of first lines
+if N1 > 1
+    line1 = line1(inds,:);
+end
+x1 =  line1(:,1);
+y1 =  line1(:,2);
+dx1 = line1(:,3);
+dy1 = line1(:,4);
+
+% extract base coordinates of second lines
+if N2 > 1
+    line2 = line2(inds,:);
+end
+x2 =  line2(:,1);
+y2 =  line2(:,2);
+dx2 = line2(:,3);
+dy2 = line2(:,4);
+
+% re-compute coordinate differences of origin points
+dx = bsxfun(@minus, line2(:,1), line1(:,1));
+dy = bsxfun(@minus, line2(:,2), line1(:,2));
+
+
+%% Compute intersection points
+
+denom = denom(inds);
+x0(inds) = (x2 .* dy2 .* dx1 - dy .* dx1 .* dx2 - x1 .* dy1 .* dx2) ./ denom ;
+y0(inds) = (dx .* dy1 .* dy2 + y1 .* dx1 .* dy2 - y2 .* dx2 .* dy1) ./ denom ;
+
 % concatenate result
-point = [x0' y0'];
+point = [x0 y0];
