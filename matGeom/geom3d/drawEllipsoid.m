b9fe464d88d835b8359ea5652fcf2451cc367023
David Legland <david.legland@nantes.inra.fr>
2012-07-20 08:45:17 +0000

    geom3d: update drawEllipsoid.m

--- matGeom/geom3d/drawEllipsoid.m
+++ matGeom/geom3d/drawEllipsoid.m
@@ -1,4 +1,4 @@
-function varargout = drawEllipsoid(varargin)
+function varargout = drawEllipsoid(elli, varargin)
 %DRAWELLIPSOID Draw a 3D ellipsoid
 %
 %   drawEllipsoid(ELLI)
@@ -8,10 +8,21 @@ function varargout = drawEllipsoid(varargin)
 %   lengths of the ellipsoid main axes, and PHI THETA PSI are Euler angles
 %   representing ellipsoid orientation, in degrees.
 %
+%   drawEllipsoid(..., 'drawEllipses', true)
+%   Also displays the main 3D ellipses corresponding to XY, XZ and YZ
+%   planes.
+%
+%
 %   Example
-%   figure; hold on;
-%   drawEllipsoid([10 20 30   50 30 10   5 10 0], 'FaceColor', 'g');
-%   axis equal;
+%     figure; hold on;
+%     drawEllipsoid([10 20 30   50 30 10   5 10 0]);
+%     axis equal;
+%
+%     figure; hold on;
+%     elli = [[10 20 30   50 30 10   5 10 0];
+%     drawEllipsoid(elli, 'FaceColor', 'r', ...
+%         'drawEllipses', true, 'EllipseColor', 'b', 'EllipseWidth', 3);
+%     axis equal;
 %
 %   See also
 %   spheres, drawSphere, inertiaEllipsoid, ellipsoid
@@ -24,76 +35,75 @@ function varargout = drawEllipsoid(varargin)
 % Copyright 2011 INRA - Cepia Software Platform.
 
 
-% process input options: when a string is found, assumes this is the
-% beginning of options
-options = {'FaceColor', 'g', 'linestyle', 'none'};
-for i = 1:length(varargin)
-    if ischar(varargin{i})
-        options = [options(1:end) varargin(i:end)];
-        varargin = varargin(1:i-1);
-        break;
-    end
-end
-
-% default ellipsoid orientation
-ellPhi   = 0;
-ellTheta = 0;
-ellPsi   = 0;
-
-% Parse the input (try to extract center coordinates and radius)
-if isempty(varargin)
-    % no input: assumes ellipsoid with defualt shape
-    xc = 0;	yc = 0; zc = 0;
-    a = 5; b = 4; c = 3;
-    
-elseif length(varargin) == 1
-    % one argument: concatenates center and radius
-    elli = varargin{1};
-    xc = elli(:,1);
-    yc = elli(:,2);
-    zc = elli(:,3);
-    a  = elli(:,4);
-    b  = elli(:,5);
-    c  = elli(:,6);
-    k = pi / 180;
-    ellPhi   = elli(:,7) * k;
-    ellTheta = elli(:,8) * k;
-    ellPsi   = elli(:,9) * k;
-    
-elseif length(varargin) == 2
-    % two arguments, corresponding to center and radius vector
-    center = varargin{1};
-    xc = center(:,1);
-    yc = center(:,2);
-    zc = center(:,3);
-    r  = varargin{2};
-    a  = r(:,1);
-    b  = r(:,2);
-    c  = r(:,3);
-   
-else
-    error('drawEllipsoid: incorrect input arguments');
-end
+%% Default values
 
 % number of meridians
 nPhi    = 32;
-ind = strmatch('nphi', lower(options(1:2:end)));
-if ~isempty(ind)
-    ind = ind(1);
-    nPhi = options{2*ind};
-    options(2*ind-1:2*ind) = [];
-end
-    
+
 % number of parallels
 nTheta  = 16;
-ind = strmatch('ntheta', lower(options(1:2:end)));
-if ~isempty(ind)
-    ind = ind(1);
-    nTheta = options{2*ind};
-    options(2*ind-1:2*ind) = [];
+
+% settings for drawing ellipses
+drawEllipses = false;
+ellipseColor = 'k';
+ellipseWidth = 1;
+
+
+%% Extract input arguments
+
+% Parse the input (try to extract center coordinates and radius)
+if nargin == 1
+    % no input: assumes ellipsoid with default shape
+    elli = [0 0 0 5 4 3 0 0 0];
 end
 
-% compute spherical coordinates
+% default set of options for drawing meshes
+options = {'FaceColor', 'g', 'linestyle', 'none'};
+
+while length(varargin) > 1
+    switch lower(varargin{1})
+        case 'nphi'
+            nPhi = varargin{2};
+            
+        case 'ntheta'
+            nTheta = varargin{2};
+
+        case 'drawellipses'
+            drawEllipses = varargin{2};
+            
+        case 'ellipsecolor'
+            ellipseColor = varargin{2};
+
+        case 'ellipsewidth'
+            ellipseWidth = varargin{2};
+
+        otherwise
+            % assumes this is drawing option
+            options = [options varargin(1:2)]; %#ok<AGROW>
+    end
+
+    varargin(1:2) = [];
+end
+
+
+%% Parse numerical inputs
+
+% Extract ellipsoid parameters
+xc  = elli(:,1);
+yc  = elli(:,2);
+zc  = elli(:,3);
+a   = elli(:,4);
+b   = elli(:,5);
+c   = elli(:,6);
+k   = pi / 180;
+ellPhi   = elli(:,7) * k;
+ellTheta = elli(:,8) * k;
+ellPsi   = elli(:,9) * k;
+
+
+%% parametrisation
+
+% spherical coordinates
 theta   = linspace(0, pi, nTheta+1);
 phi     = linspace(0, 2*pi, nPhi+1);
 
@@ -102,6 +112,30 @@ sintheta = sin(theta);
 x = cos(phi') * sintheta;
 y = sin(phi') * sintheta;
 z = ones(length(phi),1) * cos(theta);
+
+if drawEllipses
+    % parametrisation for ellipses
+    nVertices = 120;
+    t = linspace(0, 2*pi, nVertices+1);
+
+    % XY circle
+    xc1 = cos(t');
+    yc1 = sin(t');
+    zc1 = zeros(size(t'));
+
+    % XZ circle
+    xc2 = cos(t');
+    yc2 = zeros(size(t'));
+    zc2 = sin(t');
+
+    % YZ circle
+    xc3 = zeros(size(t'));
+    yc3 = cos(t');
+    zc3 = sin(t');
+end
+
+
+%% Coordinates computation
 
 % convert unit basis to ellipsoid basis
 sca     = createScaling3d(a, b, c);
@@ -116,10 +150,27 @@ trans   = tra * rotZ * rotY * rotX * sca;
 % transform mesh vertices
 [x y z] = transformPoint3d(x, y, z, trans);
 
+if drawEllipses
+    [xc1 yc1 zc1] = transformPoint3d(xc1, yc1, zc1, trans);
+    [xc2 yc2 zc2] = transformPoint3d(xc2, yc2, zc2, trans);
+    [xc3 yc3 zc3] = transformPoint3d(xc3, yc3, zc3, trans);
+end
+
+
+%% Drawing 
+
+ellipseOptions = {'color', ellipseColor, 'LineWidth', ellipseWidth};
+
 % Process output
 if nargout == 0
     % no output: draw the ellipsoid
     surf(x, y, z, options{:});
+
+    if drawEllipses
+        plot3(xc1, yc1, zc1, ellipseOptions{:});
+        plot3(xc2, yc2, zc2, ellipseOptions{:});
+        plot3(xc3, yc3, zc3, ellipseOptions{:});
+    end
     
 elseif nargout == 1
     % one output: draw the ellipsoid and return handle 
@@ -130,5 +181,13 @@ elseif nargout == 3
     varargout{1} = x; 
     varargout{2} = y; 
     varargout{3} = z; 
+    
+elseif nargout == 4 && drawEllipses
+    % Also returns handles to ellipses
+    varargout{1} = surf(x, y, z, options{:});
+    varargout{2} = plot3(xc1, yc1, zc1, ellipseOptions{:});
+    varargout{3} = plot3(xc2, yc2, zc2, ellipseOptions{:});
+    varargout{4} = plot3(xc3, yc3, zc3, ellipseOptions{:});
+    
 end
 
