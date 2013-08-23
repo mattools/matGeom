4725115103ce7df61f1abc2cffef5ef1da88cc0c
David Legland <david.legland@nantes.inra.fr>
2013-08-23 10:34:59 +0000

    meshes3d/meshVolume.m: speed improvement

--- matGeom/meshes3d/meshVolume.m
+++ matGeom/meshes3d/meshVolume.m
@@ -12,31 +12,35 @@ function vol = meshVolume(vertices, edges, faces)
 %
 %   See also
 %   meshes3d, meshSurfaceArea, tetrahedronVolume
-%
+
 % ------
 % Author: David Legland
 % e-mail: david.legland@grignon.inra.fr
 % Created: 2012-10-01,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2012 INRA - Cepia Software Platform.
 
+% HISTORY
+% 2013-08-16 speed improvement by Sven Holcombe
+
 % check input number
 if nargin == 2
     faces = edges;
 end
 
+% ensure mesh has triangle faces
 faces = triangulateFaces(faces);
 
+% initialize an array of volume
 nFaces = size(faces, 1);
 vols = zeros(nFaces, 1);
 
-% a 3x3 matrix representing repetition of centroid
-centroid = repmat(mean(vertices), 3, 1);
+% Shift all vertices to the mesh centroid
+vertices = bsxfun(@minus, vertices, mean(vertices,1));
 
 % compute volume of each tetraedron
 for i = 1:nFaces
     % consider the tetrahedron formed by face and mesh centroid
-    face = faces(i, :);
-    tetra = vertices(face, :) - centroid;
+    tetra = vertices(faces(i, :), :);
     
     % volume of current tetrahedron
     vols(i) = det(tetra) / 6;
