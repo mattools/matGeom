24100a46e14b5ccf7724d743f42ebae112974ba9
David Legland <david.legland@nantes.inra.fr>
2014-01-20 13:01:06 +0000

    graphs: add grShortestPath.m, implementing Dijkstra algorithm

---
+++ tests/graphs/test_grPropagateDistance.m
@@ -0,0 +1,33 @@
+function test_suite = test_grPropagateDistance(varargin) %#ok<STOUT>
+%TEST_GRPROPAGATEDISTANCE  Test case for the file grPropagateDistance
+%
+%   Test case for the file grPropagateDistance
+
+%   Example
+%   test_grPropagateDistance
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2014-01-13,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2014 INRA - Cepia Software Platform.
+
+initTestSuite;
+
+function test_Simple %#ok<*DEFNU>
+% propagate distance on a graph where a branch has smaller nodes but larger
+% cumulated distance
+
+% create graph structure
+n0 = [10 10;20 10; 30 10;40 10;50 10;30 40];
+e0 = [1 2;1 6; 2 3;3 4;4 5;5 6];
+% weights of edges
+l0 = [2 5 2 2 2 5]';
+% distance of node number 5 should be 8 (four times 2)
+dists = grPropagateDistance(n0, e0, 1, l0);
+assertEqual(8, dists(5));
+
+
---
+++ tests/graphs/test_grShortestPath.m
@@ -0,0 +1,33 @@
+function test_suite = test_grShortestPath(varargin) %#ok<STOUT>
+%TEST_GRSHORTESTPATH  Test case for the file grShortestPath
+%
+%   Test case for the file grShortestPath
+
+%   Example
+%   test_grShortestPath
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2014-01-13,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2014 INRA - Cepia Software Platform.
+
+initTestSuite;
+
+
+function test_Simple %#ok<*DEFNU>
+% propagate distance on a graph where a branch has smaller nodes but larger
+% cumulated distance
+
+% create graph structure
+n0 = [10 10;20 10; 30 10;40 10;50 10;30 40];
+e0 = [1 2;1 6; 2 3;3 4;4 5;5 6];
+% weights of edges
+l0 = [2 5 2 2 2 5]';
+path = grShortestPath(n0, e0, 1, 5, l0);
+
+expPath = [1 2 3 4 5]';
+assertElementsAlmostEqual(expPath, path);
