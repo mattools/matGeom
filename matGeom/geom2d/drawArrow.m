c3afea5a191d11a1cb6e5b628bb5e02ad8d29e43
David Legland <david.legland@nantes.inra.fr>
2014-10-06 09:11:04 +0000

    geom2d/drawArrow.m: use relative size for the head of the arrow, keep hold status of axis

--- matGeom/geom2d/drawArrow.m
+++ matGeom/geom2d/drawArrow.m
@@ -1,16 +1,16 @@
 function varargout = drawArrow(varargin)
 %DRAWARROW Draw an arrow on the current axis
 %   
-%   DRAWARROW(x1, y1, x2, y2) 
+%   drawArrow(x1, y1, x2, y2) 
 %   draws an arrow between the points (x1 y1) and (x2 y2).
 %
-%   DRAWARROW([x1 y1 x2 y2])
+%   drawArrow([x1 y1 x2 y2])
 %   gives argument as a single array.
 %
-%   DRAWARROW(..., L, W)
+%   drawArrow(..., L, W)
 %   specifies length and width of the arrow.
 %
-%   DRAWARROW(..., L, W, TYPE)
+%   drawArrow(..., L, W, TYPE)
 %   also specifies arrow type. TYPE can be one of the following :
 %   0: draw only two strokes
 %   1: fill a triangle
@@ -19,7 +19,7 @@ function varargout = drawArrow(varargin)
 %   Arguments can be single values or array of size N-by-1. In this case,
 %   the function draws multiple arrows.
 %
-%   H = DRAWARROW(...) 
+%   H = drawArrow(...) 
 %   return handle(s) to created arrow elements
 %
 %   Example
@@ -69,13 +69,13 @@ end
 % default values
 l = 10 * size(size(x1));
 w = 5 * ones(size(x1));
-h = zeros(size(x1));
+r = zeros(size(x1));
 
 % exctract length of arrow
 if ~isempty(varargin)
     l = varargin{1};
-    if length(x1)>length(l)
-        l = l(1)*ones(size(x1));
+    if length(x1) > length(l)
+        l = l(1) * ones(size(x1));
     end
 end
 
@@ -89,43 +89,51 @@ end
 
 % extract 'ratio' of arrow
 if length(varargin) > 2
-    h = varargin{3};
-    if length(x1) > length(h)
-        h = h(1) * ones(size(x1));
+    r = varargin{3};
+    if length(x1) > length(r)
+        r = r(1) * ones(size(x1));
     end
 end
 
 hold on;
+oldHold = ishold(gca);
+if ~oldHold
+    hold on;
+end
 axis equal;
 
 % angle of the edge
 theta = atan2(y2-y1, x2-x1);
 
 % point on the 'left'
-xa1 = x2 - l.*cos(theta) - w.*sin(theta)/2;
-ya1 = y2 - l.*sin(theta) + w.*cos(theta)/2;
+xa1 = x2 - r.*l.*cos(theta) - w.*sin(theta)/2;
+ya1 = y2 - r.*l.*sin(theta) + w.*cos(theta)/2;
 % point on the 'right'
-xa2 = x2 - l.*cos(theta) + w.*sin(theta)/2;
-ya2 = y2 - l.*sin(theta) - w.*cos(theta)/2;
+xa2 = x2 - r.*l.*cos(theta) + w.*sin(theta)/2;
+ya2 = y2 - r.*l.*sin(theta) - w.*cos(theta)/2;
 % point on the middle of the arrow
-xa3 = x2 - l.*cos(theta).*h;
-ya3 = y2 - l.*sin(theta).*h;
+xa3 = x2 - r.*l.*cos(theta);
+ya3 = y2 - r.*l.*sin(theta);
 
 % draw main edge
 h1 = line([x1'; x2'], [y1'; y2'], 'color', [0 0 1]);
 
 % draw only 2 wings
-ind = find(h==0);
+ind = find(r==0);
 h2 = line([xa1(ind)'; x2(ind)'], [ya1(ind)'; y2(ind)'], 'color', [0 0 1]);
 h3 = line([xa2(ind)'; x2(ind)'], [ya2(ind)'; y2(ind)'], 'color', [0 0 1]);
 
 % draw a full arrow
-ind = find(h~=0);
+ind = find(r~=0);
 h4 = patch([x2(ind) xa1(ind) xa3(ind) xa2(ind) x2(ind)]', ...
     [y2(ind) ya1(ind) ya3(ind) ya2(ind) y2(ind)]', [0 0 1]);
-h4(h==0) = 0;
+h4(r==0) = 0;
 
 % format output arguments
 if nargout > 0
     varargout{1} = [h1 h2 h3 h4];
+end
+
+if ~oldHold
+    hold off;
 end
