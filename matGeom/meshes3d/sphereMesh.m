1550ba3c5bdf70c4dbf1dcea3f2759a286e13642
David Legland <david.legland@nantes.inra.fr>
2015-02-24 15:58:42 +0000

    geom3D: fix bug in sphereMesh.m

--- matGeom/meshes3d/sphereMesh.m
+++ matGeom/meshes3d/sphereMesh.m
@@ -1,11 +1,16 @@
 function varargout = sphereMesh(sphere, varargin)
 %SPHEREMESH  Create a 3D mesh representing a sphere
 %
-%   [V F] = sphereMesh(S)
+%   [V, F] = sphereMesh(S)
 %   Creates a 3D mesh representing the sphere S given by [xc yc zy r].
 %
-%   [V F] = sphereMesh();
+%   [V, F] = sphereMesh();
 %   Assumes sphere is the unit sphere centered at the origin.
+%
+%   [V, F] = sphereMesh(S, 'nTheta', NT, 'nPhi', NP);
+%   Specifies the number of discretisation steps for the meridians and the
+%   parallels.
+%
 %
 %   Example
 %     s = [10 20 30 40];
@@ -14,7 +19,7 @@ function varargout = sphereMesh(sphere, varargin)
 %     view(3);axis equal; light; lighting gouraud;
 %
 %   See also
-%     meshes3d, cylinder, surfToMesh, drawSphere
+%     meshes3d, cylinderMesh, surfToMesh, drawSphere
 %
 
 % ------
@@ -27,17 +32,30 @@ if nargin == 0
     sphere = [0 0 0 1];
 end
 
+% number of meridians
+nPhi    = 32;
+    
+% number of parallels
+nTheta  = 16;
+
+% process input arguments
+while length(varargin) > 1
+    paramName = varargin{1};
+    switch lower(paramName)
+        case 'ntheta', nTheta = varargin{2};
+        case 'nphi', nPhi = varargin{2};
+        otherwise
+            error(['Could not recognise parameter: ' paramName]);
+    end
+    varargin(1:2) = [];
+end
+
 % extract sphere data
 xc = sphere(:,1);
 yc = sphere(:,2);
 zc = sphere(:,3);
 r  = sphere(:,4);
 
-% number of meridians
-nPhi    = 32;
-    
-% number of parallels
-nTheta  = 16;
 
 % compute spherical coordinates
 theta   = linspace(0, pi, nTheta+1);
@@ -50,7 +68,7 @@ y = yc + sin(phi') * sintheta * r;
 z = zc + ones(length(phi),1) * cos(theta) * r;
 
 % convert to FV mesh
-[vertices, faces] = surfToMesh(x, y, z, 'xperiodic', true);
+[vertices, faces] = surfToMesh(x, y, z, 'yperiodic', true);
 
 % format output
 varargout = formatMeshOutput(nargout, vertices, faces);
