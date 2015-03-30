6c6d5d8b4138f25814877f4bb4a3ea4fddc1e46a
David Legland <david.legland@nantes.inra.fr>
2015-03-30 11:49:01 +0000

    geom3d/drawVector3d.m: draw array of vectors with correct length

--- matGeom/geom3d/drawVector3d.m
+++ matGeom/geom3d/drawVector3d.m
@@ -2,6 +2,12 @@ function varargout = drawVector3d(pos, vect, varargin)
 %DRAWVECTOR3D Draw vector at a given position
 %
 %   drawVector3d(POS, VECT)
+%   Draws the vector VECT starting at the position POS. Both VECT and POS
+%   are N-by-3 arrays.
+%
+%   drawVector3d(..., PNAME, PVALUE)
+%   Specifies additional optional parameters that will be given to the
+%   quiver3 function.
 %
 %   Example
 %     figure; hold on;
@@ -11,16 +17,17 @@ function varargout = drawVector3d(pos, vect, varargin)
 %     view(3);
 %
 %   See also
-%   quiver3
+%   vectors3d, quiver3
 %
+
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@nantes.inra.fr
 % Created: 2011-12-19,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2011 INRA - Cepia Software Platform.
 
 h = quiver3(pos(:, 1), pos(:, 2), pos(:, 3), ...
-    vect(:, 1), vect(:, 2), vect(:, 3), varargin{:});
+    vect(:, 1), vect(:, 2), vect(:, 3), 0, varargin{:});
 
 % format output
 if nargout > 0
