0b2d80637b7aadc55677dd093f873920662c8dd3
JuanPi Carbajal <juanpi@dwengo.org>
2016-05-23 00:35:26 +0200

    drawArrow: simplify code, improve calculation, decouple ration and head type, improve output arguments, extend docs

--- matGeom/geom2d/drawArrow.m
+++ matGeom/geom2d/drawArrow.m
@@ -20,7 +20,10 @@ function varargout = drawArrow(varargin)
 %   the function draws multiple arrows.
 %
 %   H = drawArrow(...) 
-%   return handle(s) to created arrow elements
+%   return handle(s) to created arrow elements.
+%   The handles are returned in a structure with the fields
+%   'body', 'wing' and 'head' containing the handles to the different
+%   parts of the arrow(s).
 %
 %   Example
 %     t = linspace(0, 2*pi, 200);
@@ -40,6 +43,7 @@ function varargout = drawArrow(varargin)
 
 %   HISTORY
 %   2014-09-17 fix managment of handle values as suggested by Benoit Botton
+%   2016-05-23 Improve codee and reduce calculations (by JuanPi Carbajal)
 
 if isempty(varargin)
     error('should specify at least one argument');
@@ -65,34 +69,24 @@ else
     error('MatGeom:drawArrow:invalidArgumentNumber', ...
         'wrong number of arguments, please read the doc');
 end
+N     = size (x1, 1);
 
-% default values
-l = 10 * size(size(x1));
-w = 5 * ones(size(x1));
-r = zeros(size(x1));
+# default values
+l = 10  * ones (N, 1); % Body length
+w = 5   * ones (N, 1); % Head width
+r = 0.1 * ones (N, 1); % Head to body ratio
+h = zeros (N, 1);      % Head type
 
-% exctract length of arrow
-if ~isempty(varargin)
-    l = varargin{1};
-    if length(x1) > length(l)
-        l = l(1) * ones(size(x1));
-    end
-end
-
-% extract width of arrow
-if length(varargin)>1
-    w = varargin{2};
-    if length(x1) > length(w)
-        w = w(1) * ones(size(x1));
-    end
-end
-
-% extract 'ratio' of arrow
-if length(varargin) > 2
-    r = varargin{3};
-    if length(x1) > length(r)
-        r = r(1) * ones(size(x1));
-    end
+if ~isempty (varargin)
+  # Parse paramters
+  k      = length (varargin);
+  vartxt = 'lwrh';
+  cmd    = ['%s = varargin{%d}; %s = %s(:);' ...
+            'if length (%s) < N; %s = %s(1) * ones (N , 1); end'];
+  for i = 1:k
+    v = vartxt(i);
+    eval (sprintf (cmd, v, i, v, v, v, v, v));
+  end
 end
 
 hold on;
@@ -105,33 +99,47 @@ axis equal;
 % angle of the edge
 theta = atan2(y2-y1, x2-x1);
 
+rl = r .* l;
+rh = r .* h;
+cT = cos (theta);
+sT = sin (theta);
 % point on the 'left'
-xa1 = x2 - r.*l.*cos(theta) - w.*sin(theta)/2;
-ya1 = y2 - r.*l.*sin(theta) + w.*cos(theta)/2;
+xa1 = x2 - rl .* cT - w .* sT / 2;
+ya1 = y2 - rl .* sT + w .* cT / 2;
 % point on the 'right'
-xa2 = x2 - r.*l.*cos(theta) + w.*sin(theta)/2;
-ya2 = y2 - r.*l.*sin(theta) - w.*cos(theta)/2;
+xa2 = x2 - rl .* cT + w .* sT / 2;
+ya2 = y2 - rl .* sT - w .* cT / 2;
 % point on the middle of the arrow
-xa3 = x2 - r.*l.*cos(theta);
-ya3 = y2 - r.*l.*sin(theta);
+xa3 = x2 - rh .* cT;
+ya3 = y2 - rh .* sT;
 
 % draw main edge
-h1 = line([x1'; x2'], [y1'; y2'], 'color', [0 0 1]);
+tmp         = line ([x1.'; x2.'], [y1.'; y2.'], 'color', [0 0 1]);
+handle.body = tmp;
 
 % draw only 2 wings
-ind = find(r==0);
-h2 = line([xa1(ind)'; x2(ind)'], [ya1(ind)'; y2(ind)'], 'color', [0 0 1]);
-h3 = line([xa2(ind)'; x2(ind)'], [ya2(ind)'; y2(ind)'], 'color', [0 0 1]);
+ind = find (h == 0);
+if ~isempty (ind)
+  tmp              = line ([xa1(ind).'; x2(ind).'], [ya1(ind).'; y2(ind).'], ...
+                           'color', [0 0 1]);
+  handle.wing(:,1) = tmp;
+
+  tmp              = line([xa2(ind).'; x2(ind).'], [ya2(ind).'; y2(ind).'], ...
+                           'color', [0 0 1]);
+  handle.wing(:,2) = tmp;
+end
 
 % draw a full arrow
-ind = find(r~=0);
-h4 = patch([x2(ind) xa1(ind) xa3(ind) xa2(ind) x2(ind)]', ...
-    [y2(ind) ya1(ind) ya3(ind) ya2(ind) y2(ind)]', [0 0 1]);
-h4(r==0) = 0;
+ind = find (h ~= 0);
+if ~isempty (ind)
+  tmp         = patch ([x2(ind) xa1(ind) xa3(ind) xa2(ind) x2(ind)].', ...
+                       [y2(ind) ya1(ind) ya3(ind) ya2(ind) y2(ind)].', [0 0 1]);
+  handle.head = tmp;
+end
 
 % format output arguments
 if nargout > 0
-    varargout{1} = [h1 h2 h3 h4];
+    varargout{1} = handle;
 end
 
 if ~oldHold
