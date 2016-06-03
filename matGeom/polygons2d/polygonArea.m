0cc62043f1347b49a041a0da51c8ec98c27a511e
dlegland <david.legland@nantes.inra.fr>
2016-06-03 16:55:08 +0200

    polygons2d/polygonArea.m: revert to previous version and adapt for polygons with holes
    there were some problems with nested loops.
    I have kept doc updates, with adaptation, as Matlab do not accept "a = b = c" statements.

--- matGeom/polygons2d/polygonArea.m
+++ matGeom/polygons2d/polygonArea.m
@@ -11,13 +11,6 @@ function area = polygonArea(poly, varargin)
 %
 %   If polygon is self-crossing, the result is undefined.
 %
-%   If the polygon contains holes (separated by nans) the result is well defined
-%   only when the outer polygon is oriented CCW and the holes are oriented CW.
-%   In this case the result is the sum of the signed areas.
-%
-%   If the input is a cell, each element is considered a polygon and the area
-%   of each one is returned in the matrix that has the same shape as the cell.
-%
 %   Examples
 %     % compute area of a simple shape
 %     poly = [10 10;30 10;30 20;10 20];
@@ -38,10 +31,10 @@ function area = polygonArea(poly, varargin)
 %     area =
 %        400
 %
-%     % Area of square with hole
-%     pccw = pcw = [0 0; 1 0; 1 1; 0 1];
-%     pcw([2 4],:) = pcw([4 2], :);
-%     polygonArea ([pc; nan(1,2); 0.5*pcw+[0.25 0.25]])
+%     % Area of unit square with 25% hole
+%     pccw = [0 0; 1 0; 1 1; 0 1];
+%     pcw = pccw([1 4 3 2], :) * .5 + .25;
+%     polygonArea ([pccw; nan(1,2); pcw])
 %     ans =
 %        0.75
 %
@@ -52,45 +45,59 @@ function area = polygonArea(poly, varargin)
 %   See also:
 %   polygons2d, polygonCentroid, drawPolygon, triangleArea
 %
+
 %   ---------
 %   author : David Legland 
 %   INRA - TPV URPOI - BIA IMASTE
 %   created the 05/05/2004.
+%
 
 %   HISTORY
 %   25/04/2005: add support for multiple polygons
 %   12/10/2007: update doc
-%   29/05/2016: Simplified code and handled holes (by JuanPi Carbajal)
 
-function A = polygonArea(px, py)
 
-    % in case of polygon sets, computes several areas
-    if iscell (px)
-         A = cellfun (@func, px);
-    else
+%% Process special cases
 
-        if nargin == 2
-            px = [px py];
-        end
-        A = func (px);
+% in case of polygon sets, computes the sum of polygon areas
+if iscell(poly)
+    area = 0;
+    for i = 1:length(poly)
+        area = area + polygonArea(poly{i});
     end
-
+    return;
 end
 
-function a = func (c)
-
-    if any (isnan (c))
-        cc = splitPolygons (c);
-        a  = cellfun (@func, cc);
-        a  = sum (a);
-    else
-        N = size (c, 1);
-        if N < 3
-            a = 0;
-        else
-            iNext = [2:N 1];
-            a     = sum (c(:,1) .* c(iNext,2) - c(iNext,1) .* c(:,2)) / 2;
-        end
-    end
-
+% check there are enough points
+if size(poly, 1) < 2
+    area = 0;
+    return;
 end
+
+% case of polygons with holes -> computes the sum of areas
+if any(isnan(poly))
+    area = sum(polygonArea(splitPolygons(poly)));
+    return;
+end
+
+
+%% Process single polygons or single rings
+
+% extract coordinates
+if nargin == 1
+    % polygon given as N-by-2 array
+    px = poly(:, 1);
+    py = poly(:, 2);
+    
+elseif nargin == 2
+    % poylgon given as two N-by-1 arrays
+    px = poly;
+    py = varargin{1};
+end
+
+% indices of next vertices
+N = length(px);
+iNext = [2:N 1];
+
+% compute area (vectorized version)
+area = sum(px .* py(iNext) - px(iNext) .* py) / 2;
