971279bd9fde2cf7170077825ce187749e61a8fa
dlegland <david.legland@inra.fr>
2017-04-05 12:03:49 +0200

    update createHomothecy.m

--- matGeom/geom2d/createHomothecy.m
+++ matGeom/geom2d/createHomothecy.m
@@ -3,6 +3,8 @@ function T = createHomothecy(point, ratio)
 %
 %   TRANS = createHomothecy(POINT, K);
 %   POINT is the center of the homothecy, K is its factor.
+%   TRANS is a 3-by-3 matrix representing the homothetic transform in
+%   homogeneous coordinates.
 %
 %   Example:
 %
@@ -12,29 +14,32 @@ function T = createHomothecy(point, ratio)
 %      pT = transformPoint (p, T);
 %      drawPolygon (p,'-b')
 %      hold on;
-%      drawPolygon (pT,'-r');
-%      drawEdge (s([1 1 1])(:),s([2 2 2])(:), pT(:,1), pT(:,2), ...
+%      drawPolygon (pT, '-r');
+%      
+%      drawEdge (p(:,1), p(:,2), pT(:,1), pT(:,2), ...
 %                'color', 'k','linestyle','--')
 %      hold off
 %      axis tight equal
 %
-%   ---------
-%   author : David Legland 
-%   INRA - TPV URPOI - BIA IMASTE
-%   created the 20/01/2005.
-%
+
+% ---------
+% Author: David Legland
+% e-mail: david.legland@inra.fr
+% INRA - TPV URPOI - BIA IMASTE
+% created the 20/01/2005.
+
 
 %   HISTORY
 %   22/04/2009: rename as createHomothecy
 %   05/04/2017: improved code by JuanPi Carbajal <ajuanpi+dev@gmail.com>
 
-  point = point (:);
-  if length (point) > 2
-    error ('Only one point accepted.');
-  end
-  if length (ratio) > 1
-    error ('Only one ratio accepted.');
-  end
+point = point(:);
+if length (point) > 2
+    error('Only one point accepted.');
+end
+if length (ratio) > 1
+    error('Only one ratio accepted.');
+end
 
-  T        = diag ([ratio ratio 1]);
-  T(1:2,3) = point .* (1 - ratio);
+T        = diag ([ratio ratio 1]);
+T(1:2,3) = point .* (1 - ratio);
