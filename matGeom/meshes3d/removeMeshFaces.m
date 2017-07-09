2b7ddb0312016eef9b87f138111efd7094c75a8c
oqilipo <xx-00-xx@web.de>
2017-07-09 15:41:07 +0200

    removeMeshFaces.m: added linear and logical indexing

--- matGeom/meshes3d/removeMeshFaces.m
+++ matGeom/meshes3d/removeMeshFaces.m
@@ -1,25 +1,26 @@
-function varargout = removeMeshFaces(v, f, fLI)
-%REMOVEMESHFACES removes faces from a mesh by a logical face index
-%   [V2, F2] = removeMeshFaces(V, F, FLI) removes faces from the mesh by
-%   the logical index FLI into faces F of the mesh. The mesh is represented 
+function varargout = removeMeshFaces(v, f, fI)
+%REMOVEMESHFACES Remove faces from a mesh by face indices
+%   [V2, F2] = removeMeshFaces(V, F, FI) removes faces from the mesh by
+%   the face indices FI into faces F of the mesh. The mesh is represented 
 %   by the vertex array V and the face array F. The result is the new set 
-%   of vertices V2 and faces F2 without the faces indexed by FLI.
+%   of vertices V2 and faces F2 without the faces indexed by FI. FI can be
+%   either a linear or a logical index.
 %
-%   [V2, F2] = removeMeshFaces(MESH, FLI) with the struct MESH containing 
+%   [V2, F2] = removeMeshFaces(MESH, FI) with the struct MESH containing 
 %   the fields "vertices" (V) and "faces" (F)
 %   
-%   MESH2 = cutFacesOffMesh(V, F, FLI) with the struct MESH2 containing the
+%   MESH2 = removeMeshFaces(V, F, FI) with the struct MESH2 containing the
 %   fields "vertices" (V2) and "faces" (F2)
 %   
-%   MESH2 = cutFacesOffMesh(MESH, FLI) with the structs MESH and MESH2 
+%   MESH2 = removeMeshFaces(MESH, FI) with the structs MESH and MESH2 
 %   containing the fields "vertices" (V, V2) and "faces" (F, F2)
 %   
 %   Example
 %     [v, f] = createSoccerBall;
 %     f = triangulateFaces(f);
-%     fLI = true(length(f),1);
-%     fLI(1:length(f)/2) = false;
-%     [v2, f2] = cutFacesOffMesh(v, f, fLI);
+%     fI = true(length(f),1);
+%     fI(1:length(f)/2) = false;
+%     [v2, f2] = removeMeshFaces(v, f, fI);
 %     drawMesh(v, f, 'faceColor', 'none', 'faceAlpha', .2);
 %     drawMesh(v2, f2, 'faceAlpha', .7);
 %     view(3); axis equal
@@ -36,21 +37,30 @@ narginchk(2,3)
 nargoutchk(1,2)
 
 if nargin == 2
-    fLI = f;
+    fI = f;
     [v, f] = parseMeshData(v);
 end
 
 p = inputParser;
-islogicalIndexToFaces = @(x) islogical(x) && isequal(length(fLI), size(f,1));
-addRequired(p,'fLI',islogicalIndexToFaces)
-parse(p, fLI);
-fLI=p.Results.fLI;
+isIndexToFaces = @(x) ...
+    (islogical(x) && isequal(length(fI), size(f,1))) || ...
+    (all(floor(x)==x) && min(x)>=1 && max(x)<=size(f,1));
+addRequired(p,'fI',isIndexToFaces)
+parse(p, fI);
+if ~islogical(p.Results.fI)
+    fI=false(size(f,1),1);
+    fI(p.Results.fI)=true;
+else
+    fI=p.Results.fI;
+end
+    
 
 % algorithm
-f2 = f(~fLI,:);
+f2 = f(~fI,:);
 [unqVertIds, ~, newVertIndices] = unique(f2);
 v2 = v(unqVertIds,:);
 f2 = reshape(newVertIndices,size(f2));
+
 
 % parse outputs
 if nargout == 1
