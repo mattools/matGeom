40b785885fe7735e607c08b6e3be1cd2ea9808f5
oqilipo <xx-00-xx@web.de>
2017-10-11 19:12:39 +0200

    calculate only what is necessary for the output

--- matGeom/meshes3d/cutMeshByPlane.m
+++ matGeom/meshes3d/cutMeshByPlane.m
@@ -1,17 +1,17 @@
 function varargout = cutMeshByPlane(v, f, plane, varargin)
 %CUTMESHBYPLANE Cut a mesh by a plane
 %   [ABOVE, IN, BELOW] = cutMeshByPlane(MESH, PLANE)
-%   where MESH, ABOVE, IN, BELOW are structs with the fields vertices and 
-%   faces, and PLANE is given as a row containing initial point and 2  
+%   where MESH, ABOVE, IN, BELOW are structs with the fields vertices and
+%   faces, and PLANE is given as a row containing initial point and 2
 %   direction vectors. ABOVE, IN, BELOW contain the corresponding parts of
 %   the input mesh.
-%   
+%
 %   [ABOVE_V, ABOVE_F, IN_V, IN_F, BELOW_V,BELOW_F] = ...
-%       cutMeshByPlane(V, F, PLANE) where V is a [NVx3] array containing 
+%       cutMeshByPlane(V, F, PLANE) where V is a [NVx3] array containing
 %   coordinates and F is a [NFx3] array containing indices of vertices of
 %   the triangular faces.
 %
-%   BELOW = cutMeshByPlane(V, F, PLANE, 'part', 'below') BELOW is a struct  
+%   BELOW = cutMeshByPlane(V, F, PLANE, 'part', 'below') BELOW is a struct
 %   with the fields vertices and faces. Other options are:
 %       'part'  -   'above': Faces above the plane
 %               -   'in'   : Faces in the plane
@@ -19,7 +19,7 @@ function varargout = cutMeshByPlane(v, f, plane, varargin)
 %
 %   [BELOW_V, BELOW_F] = cutMeshByPlane(MESH, PLANE, 'part', 'below') is
 %   possible, too.
-%   
+%
 % ---------
 % Authors: oqilipo, David Legland
 % Created: 2017-07-09
@@ -53,15 +53,29 @@ VBPl_LI = isBelowPlane(v, plane);
 
 % Logical index to three vertices of each face
 FBP_LI = VBPl_LI(f);
-
-% Faces above the plane, all three vertices == 0 -> sum has to be 0
-above = removeMeshFaces(v, f, ~(sum(FBP_LI, 2) == 0) );
-
-% Faces in the plane, 1 or 2 vertices == 0 -> sum can be 1 or 2
-inside = removeMeshFaces(v, f, ~((sum(FBP_LI, 2) > 0 & sum(FBP_LI, 2) < 3)));
-
-% Faces below the plane, all three vertices == 1 -> sum has to be 3
-below = removeMeshFaces(v, f, ~(sum(FBP_LI, 2) == 3) );
+switch nargout
+    case {1, 2}
+        switch part
+            case 'above'
+                % Faces above the plane, all three vertices == 0 -> sum has to be 0
+                above = removeMeshFaces(v, f, ~(sum(FBP_LI, 2) == 0) );
+            case 'in'
+                % Faces in the plane, 1 or 2 vertices == 0 -> sum can be 1 or 2
+                inside = removeMeshFaces(v, f, ~((sum(FBP_LI, 2) > 0 & sum(FBP_LI, 2) < 3)));
+            case 'below'
+                % Faces below the plane, all three vertices == 1 -> sum has to be 3
+                below = removeMeshFaces(v, f, ~(sum(FBP_LI, 2) == 3) );
+        end
+    case {3, 6}
+        % Faces above the plane, all three vertices == 0 -> sum has to be 0
+        above = removeMeshFaces(v, f, ~(sum(FBP_LI, 2) == 0) );
+        % Faces in the plane, 1 or 2 vertices == 0 -> sum can be 1 or 2
+        inside = removeMeshFaces(v, f, ~((sum(FBP_LI, 2) > 0 & sum(FBP_LI, 2) < 3)));
+        % Faces below the plane, all three vertices == 1 -> sum has to be 3
+        below = removeMeshFaces(v, f, ~(sum(FBP_LI, 2) == 3) );
+    otherwise
+        error('Invalid numer of output arguments')
+end
 
 
 %% Parse outputs
