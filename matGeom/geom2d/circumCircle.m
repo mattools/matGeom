fee7c32cf8491b7da369cabc96d31a58b2838179
dlegland <david.legland@inra.fr>
2017-12-01 11:02:15 +0100

    geom2d/circumCenter.m: add support for multiple point

--- matGeom/geom2d/circumCircle.m
+++ matGeom/geom2d/circumCircle.m
@@ -15,9 +15,10 @@ function varargout = circumCircle(varargin)
 %   See also
 %     circles2d, enclosingCircle, circumCenter
 %
+
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@inra.fr
 % Created: 2011-12-01,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2011 INRA - Cepia Software Platform.
 
