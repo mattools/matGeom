6f07d1d8bbdca37388559c46804c4c2c97830334
JuanPi Carbajal <ajuanpi+dev@gmail.com>
2018-09-10 17:02:21 +0200

    maint:automatic octave package built

---
+++ matGeom/__matgeom_package_register__.m
@@ -0,0 +1,39 @@
+## Copyright (C) 2016 Carnë Draug <carandraug@octave.org>
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see
+## <http:##www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {} __matgeom_package_register__ ()
+## Undocumented internal function of matgeom package.
+## @end deftypefn
+
+## PKG_ADD: __matgeom_package_register__ (1);
+## PKG_DEL: __matgeom_package_register__ (-1);
+
+function subdir_paths = __matgeom_package_register__ (loading = 0)
+
+  subdirlist = {"utils", "geom2d", "polygons2d", "graphs",...
+                "geom3d","meshes3d","polynomialCurves2d"};
+  ## Get full path, with luck we can retreive the package name from here
+  base_pkg_path = fileparts (make_absolute_filename (mfilename ("fullpath")));
+
+  subdir_paths = fullfile (base_pkg_path, subdirlist);
+  if (loading > 0)
+    addpath (subdir_paths{:});
+  elseif (loading < 0)
+    rmpath (subdir_paths{:});
+  endif
+
+endfunction
---
+++ matGeom/index.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$1
+PACKAGE=$(sed -n -e 's/^Title: *\(\w\+\)/\1/p' $DIR/DESCRIPTION)
+echo "matgeom >> " "$PACKAGE"
+
+for dir in geom3d polygons2d geom2d meshes3d utils graphs polynomialCurves2d;
+do
+ names=$(ls $DIR/$dir | sed -n -e 's/\(\w\+\).m$/ \1/p')
+ echo "$dir"
+ echo "$names"
+done
+
+
