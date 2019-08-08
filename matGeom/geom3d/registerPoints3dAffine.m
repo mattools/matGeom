8ba98b858bc08cbe3b4786c1c90f6e79fa832ca6
dlegland <david.legland@inra.fr>
2019-08-08 11:13:35 +0200

    geom3d: add fitAffineTransform3d and registerPoints3dAffine.m

--- matGeom/geom3d/registerPoints3dAffine.m
+++ matGeom/geom3d/registerPoints3dAffine.m
@@ -1,23 +1,23 @@
-function [trans, points] = registerICP(points, target, varargin)
-%REGISTERICP Fit affine transform by Iterative Closest Point algorithm.
+function [trans, points] = registerPoints3dAffine(points, target, varargin)
+% Fit 3D affine transform using iterative algorithm.
 %
-%   TRANS = registerICP(POINTS, TARGET)
+%   TRANS = registerPoints3dAffine(POINTS, TARGET)
 %   Computes the affine transform that maps the shape defines by POINTS
 %   onto the shape defined by the points TARGET. Both POINTS and TARGET are
-%   N-by-2 array of point coordinates, not necessarily the same size.
-%   The result TRANS is a 3-by-3 affine transform.
+%   N-by-3 array of point coordinates, not necessarily the same size.
+%   The result TRANS is a 4-by-4 affine transform.
 %
-%   TRANS = registerICP(POINTS, TARGET, NITER)
+%   TRANS = registerPoints3dAffine(POINTS, TARGET, NITER)
 %   Specifies the number of iterations for the algorithm.
 %
-%   [TRANS, POINTS2] = registerICP(...)
+%   [TRANS, POINTS2] = registerPoints3dAffine(...)
 %   Also returns the set of transformed points.
 %
 %   Example
-%   registerICP
+%     registerPoints3dAffine
 %
 %   See also
-%     transforms2d, fitAffineTransform2d, registerPoints3dAffine
+%     transforms3d, fitAffineTransform3d
 %
  
 % ------
@@ -27,23 +27,23 @@ function [trans, points] = registerICP(points, target, varargin)
 % Copyright 2015 INRA - Cepia Software Platform.
 
 
-nIter = 10;
+nIters = 10;
 if ~isempty(varargin)
-    nIter = varargin{1};
+    nIters = varargin{1};
 end
 
 % keep original points to transform them at each
-trans = [1 0 0;0 1 0;0 0 1];
+trans = [1 0 0 0;0 1 0 0;0 0 1 0;0 0 0 1];
 
-for i = 1:nIter
+for i = 1:nIters
     % identify target points for each source point
     inds = findClosestPoint(points, target);
     corrPoints = target(inds, :);
     
     % compute transform for current iteration
-    trans_i = fitAffineTransform2d(points, corrPoints);
+    trans_i = fitAffineTransform3d(points, corrPoints);
 
     % apply transform, and update cumulated transform
-    points = transformPoint(points, trans_i);
+    points = transformPoint3d(points, trans_i);
     trans = trans_i * trans;
 end
