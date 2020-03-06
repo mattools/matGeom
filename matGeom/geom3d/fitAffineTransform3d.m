6f472ebdb2199c4c707c101ea5cd388dc67583de
dlegland <david.legland@inra.fr>
2020-03-06 16:47:30 +0100

    geom3d/fitAffineTransform3d: change convention for source and target point sets

--- matGeom/geom3d/fitAffineTransform3d.m
+++ matGeom/geom3d/fitAffineTransform3d.m
@@ -1,7 +1,13 @@
-function trans = fitAffineTransform3d(pts1, pts2)
-%FITAFFINETRANSFORM3D Fit an affine transform using two point sets.
+function trans = fitAffineTransform3d(ref, src)
+% Compute the affine transform that best register two 3D point sets.
 %
-%   TRANS = fitAffineTransform3d(PTS1, PTS2)
+%   TRANS = fitAffineTransform3d(REF, SRC)
+%   Returns the affine transform matrix that minimizes the distance between
+%   the reference point set REF and the point set SRC after transformation.
+%   Both REF and SRC must by N-by-3 arrays with the same number of rows,
+%   and the points must be in correspondence.
+%   The function minimizes the sum of the squared distances:
+%   CRIT = sum(distancePoints3d(REF, transformPoint3d(PTS, TRANSFO)).^2);
 %
 %   Example
 %     N = 50;
@@ -14,31 +20,31 @@ function trans = fitAffineTransform3d(pts1, pts2)
 %
 %   See also
 %     transforms3d, transformPoint3d, transformVector3d,
-%     registerPoints3dAffine
+%     fitAffineTransform2d, registerPoints3dAffine
 %
 
 % ------
 % Author: David Legland
-% e-mail: david.legland@inra.fr
+% e-mail: david.legland@inrae.fr
 % Created: 2009-07-31,    using Matlab 7.7.0.471 (R2008b)
-% Copyright 2009 INRA - Cepia Software Platform.
+% Copyright 2009 INRAE - Cepia Software Platform.
 
 
 % number of points 
-N = size(pts1, 1);
-if size(pts2, 1) ~= N
+N = size(src, 1);
+if size(ref, 1) ~= N
     error('Requires the same number of points for both arrays');
 end
 
 % main matrix of the problem
-tmp = [pts1(:,1) pts1(:,2) pts1(:,3) ones(N,1)];
+tmp = [src(:,1) src(:,2) src(:,3) ones(N,1)];
 A = [...
     tmp zeros(N, 8) ; ...
     zeros(N, 4) tmp zeros(N, 4) ; ...
     zeros(N, 8) tmp ];
 
 % conditions initialisations
-B = [pts2(:,1) ; pts2(:,2) ; pts2(:,3)];
+B = [ref(:,1) ; ref(:,2) ; ref(:,3)];
 
 % compute coefficients using least square
 coefs = A\B;
