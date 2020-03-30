72ab46e48c0b6b50f376c0861a11ae0f0c680d7d
dlegland <david.legland@inra.fr>
2020-03-30 15:17:14 +0200

    polygons: polygonSubcurve and polylineSubcurve.m now also return indices of selected vertices

--- matGeom/polygons2d/polylineSubcurve.m
+++ matGeom/polygons2d/polylineSubcurve.m
@@ -1,30 +1,36 @@
-function res = polylineSubcurve(poly, t0, t1)
-%POLYLINESUBCURVE Extract a portion of a polyline.
+function [res, inds] = polylineSubcurve(poly, t0, t1)
+% Extract a portion of a polyline.
 %
 %   POLY2 = polylineSubcurve(POLYLINE, POS0, POS1)
 %   Create a new polyline, by keeping vertices located between positions
 %   POS0 and POS1, and adding points corresponding to positions POS0 and
 %   POS1 if they are not already vertices.
 %
+%   [POLY2, INDS] = polylineSubcurve(POLYLINE, POS0, POS1)
+%   Also returns the indices of the original polyline that were selected.
+%   The size of the array INDS may be smaller than the array POLY, due to
+%   the addition of new vertices at the extremities.
+%
 %   Example
-%   Nv = 100;
-%   poly = circleAsPolygon([10 20 30], Nv);
-%   poly2 = polylineSubcurve(poly, 15, 65);
-%   drawCurve(poly2);
+%     Nv = 100;
+%     poly = circleAsPolygon([10 20 30], Nv);
+%     poly2 = polylineSubcurve(poly, 15, 65);
+%     drawCurve(poly2);
 %
 %   See also
-%   polygons2d, polygonSubCurve
+%     polygons2d, polygonSubCurve
 %
+
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@inrae.fr
 % Created: 2009-04-30,    using Matlab 7.7.0.471 (R2008b)
-% Copyright 2009 INRA - Cepia Software Platform.
+% Copyright 2009 INRAE - Cepia Software Platform.
 
 % number of vertices
 Nv = size(poly, 1);
 
-if t0<t1
+if t0 < t1
     % format positions
     t0 = max(t0, 0);
     t1 = min(t1, Nv-1);
@@ -35,11 +41,13 @@ ind0 = ceil(t0)+1;
 ind1 = floor(t1)+1;
 
 % get the portion of polyline between 2 extremities
-if t0<t1
-    res = poly(ind0:ind1, :);
+if t0 < t1
+    inds = ind0:ind1;
 else
-    res = poly([ind0:end 1:ind1], :);
+    inds = [ind0:Nv 1:ind1];
 end
+
+res = poly(inds, :);
 
 % add first point if it is not already a vertex
 if t0 ~= ind0-1
@@ -47,7 +55,7 @@ if t0 ~= ind0-1
 end
 
 % add last point if it is not already a vertex
-if t1~=ind1-1
+if t1 ~= ind1-1
     res = [res; polylinePoint(poly, t1)];
 end
     
