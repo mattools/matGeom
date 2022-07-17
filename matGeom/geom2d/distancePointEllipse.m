d9f991946b1a5c12410740aac6ad7de59047ea74
dlegland <david.legland@inrae.fr>
2022-07-17 22:43:03 +0200

    geom2d: add distancePointEllipse.m, projPointOnEllipse.m

---
+++ matGeom/geom2d/distancePointEllipse.m
@@ -0,0 +1,41 @@
+function [dist, proj] = distancePointEllipse(point, elli)
+% Distance from a point to an ellipse.
+%
+%   DIST = distancePointEllipse(POINT, ELLI)
+%   Computes the Euclidean distance between the point POINT and the ellipse
+%   ELLI.
+%   POINT may also be a N-by-2 array of point coordinates. In that case the
+%   result is a N-by-1 array of distances.
+%
+%   [DIST, PROJ] = distancePointEllipse(POINT, ELLI)
+%   Also return the coordinates of the projection of the point onto the
+%   ellipse. PROJ has same dimensions as the array POINT.
+%
+%   Example
+%     % create an ellipse
+%     elli = [50 50 40 30 20];
+%     % generate points along a regular grid
+%     [x, y] = meshgrid(1:100, 1:100);
+%     pts = [x(:) y(:)];
+%     % compute distance map
+%     distMap = reshape(distancePointEllipse(pts, elli), size(x));
+%     figure; imshow(distMap, []); colormap parula;
+%
+%
+%   References:
+%     https://blog.chatfield.io/simple-method-for-distance-to-ellipse/
+%
+%   See also
+%     ellipses2d, projPointOnEllipse, distancePoints
+%
+
+% ------
+% Author: David Legland
+% e-mail: david.legland@inrae.fr
+% INRAE - BIA Research Unit - BIBS Platform (Nantes)
+% Created: 2022-07-17,    using Matlab 9.12.0.1884302 (R2022a)
+% Copyright 2022 INRAE.
+
+proj = projPointOnEllipse(point, elli);
+
+dist = distancePoints(point, proj, 'diag');
---
+++ matGeom/geom2d/projPointOnEllipse.m
@@ -0,0 +1,88 @@
+function proj = projPointOnEllipse(point, elli)
+% Project of a point orthogonally onto an ellipse.
+%
+%   PROJ = projPointOnEllipse(PT, ELLI)
+%   Computes the (orthogonal) projection of point PT onto the ellipse given
+%   by ELLI.
+%
+%
+%   Example
+%     % create an ellipse and a point
+%     elli = [50 50 40 20 30];
+%     pt = [60 10];
+%     % display reference figures
+%     figure; hold on; drawEllipse(elli, 'b');
+%     axis equal; axis([0 100 0 100]);
+%     drawPoint(pt, 'bo');
+%     % compute projected point
+%     proj = projPointOnEllipse(pt, elli);
+%     drawEdge([pt proj], 'b');
+%     drawPoint(proj, 'k*');
+%
+%   See also
+%     ellipses2d, distancePointEllipse, drawEllipse
+%
+ 
+% ------
+% Author: David Legland
+% e-mail: david.legland@inrae.fr
+% INRAE - BIA Research Unit - BIBS Platform (Nantes)
+% Created: 2022-07-17,    using Matlab 9.12.0.1884302 (R2022a)
+% Copyright 2022 INRAE.
+
+% defaults
+nMaxIters = 4;
+
+% compute transform to centered axis-aligned ellipse
+center = elli(1:2);
+theta = deg2rad(elli(5));
+transfo = createRotation(-theta) * createTranslation(-center);
+pointT = transformPoint(point, transfo);
+
+% retrieve ellipse semi axis lengths
+a = elli(3);
+b = elli(4);
+
+% keep absolute values
+px = abs(pointT(:,1));
+py = abs(pointT(:,2));
+
+% initial guess of solution
+tx = 0.707;
+ty = 0.707;
+
+% iterate
+for i = 1:nMaxIters
+    x = a * tx;
+    y = b * ty;
+
+    ex = (a*a - b*b) * power(tx, 3) / a;
+    ey = (b*b - a*a) * power(ty, 3) / b;
+
+    rx = x - ex;
+    ry = y - ey;
+
+    qx = px - ex;
+    qy = py - ey;
+
+    r = hypot(ry, rx);
+    q = hypot(qy, qx);
+
+    tx = min(1, max(0, (qx .* r ./ q + ex) / a));
+    ty = min(1, max(0, (qy .* r ./ q + ey) / b));
+    t = hypot(ty, tx);
+    tx = tx ./ t;
+    ty = ty ./ t;
+
+end
+
+% computes coordinates of projection
+projX = a * tx;
+projY = b * ty;
+
+% fix sign
+projX(pointT(:,1) < 0) = -projX(pointT(:,1) < 0);
+projY(pointT(:,2) < 0) = -projY(pointT(:,2) < 0);
+
+% project pack to basis of original ellipse
+proj = transformPoint([projX projY], inv(transfo));
