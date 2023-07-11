4b07947a975b2ce3c87ab254ff3a7fabbc6aaaa0
dlegland <david.legland@inrae.fr>
2023-07-11 09:22:53 +0200

    geom2d/edgePosition: add 'diag' option

--- matGeom/geom2d/edgePosition.m
+++ matGeom/geom2d/edgePosition.m
@@ -1,17 +1,17 @@
-function pos = edgePosition(point, edge)
+function pos = edgePosition(point, edge, varargin)
 %EDGEPOSITION Return position of a point on an edge.
 %
 %   POS = edgePosition(POINT, EDGE);
 %   Computes position of point POINT on the edge EDGE, relative to the
 %   position of edge vertices.
 %   EDGE has the form [x1 y1 x2 y2],
-%   POINT has the form [x y], and is assumed to belong to edge.
-%   The result POS has the following meaning:
+%   POINT has the form [x y], and is assumed to belong to edge supporting
+%   line. The result POS has the following meaning:
 %     POS < 0:      POINT is located before the first vertex
 %     POS = 0:      POINT is located on the first vertex
 %     0 < POS < 1:  POINT is located between the 2 vertices (on the edge)
 %     POS = 1:      POINT is located on the second vertex
-%     POS < 0:      POINT is located after the second vertex
+%     POS > 1:      POINT is located after the second vertex
 %
 %   POS = edgePosition(POINT, EDGES);
 %   If EDGES is an array of NL edges, return NE positions, corresponding to
@@ -26,33 +26,43 @@ function pos = edgePosition(point, edge)
 %   return an array of [NP NE] position, corresponding to each couple
 %   point-edge.
 %
+%   POS = edgePosition(POINTS, EDGES, 'diag');
+%   When POINTS and EDGES are two arrays with same number of rows, returns
+%   single column vector corresponding to each pair of point-edge.
+%
 %   See also 
-%   edges2d, createEdge, isPointOnEdge
+%     edges2d, createEdge, isPointOnEdge, edgeToLine
 %
 
 % ------
 % Author: David Legland
-% E-mail: david.legland@nantes.inra.fr
+% E-mail: david.legland@inrae.fr
 % Created: 2004-05-25
 % Copyright 2004-2022 INRA - Cepia Software Platform
+
+% parse input options
+diag = false;
+if ~isempty(varargin) && strcmp(varargin{1}, 'diag')
+    diag = true;
+end
 
 % number of points and of edges
 nEdges = size(edge, 1);
 nPoints = size(point, 1);
 
-if nPoints == nEdges
-    dxe = (edge(:, 3) - edge(:,1))';
-    dye = (edge(:, 4) - edge(:,2))';
-    dxp = point(:, 1) - edge(:, 1);
-    dyp = point(:, 2) - edge(:, 2);
-
+if nPoints == nEdges && diag
+    % reshape into N-by-1 arrays
+    dxe = (edge(:,3) - edge(:,1));
+    dye = (edge(:,4) - edge(:,2));
+    dxp = point(:,1) - edge(:,1);
+    dyp = point(:,2) - edge(:,2);
 else
-    % expand one of the arrays to have the same size
+    % reshape arrays to result in NP-by-NE arrays
     dxe = (edge(:,3) - edge(:,1))';
     dye = (edge(:,4) - edge(:,2))';
     dxp = bsxfun(@minus, point(:,1), edge(:,1)');
     dyp = bsxfun(@minus, point(:,2), edge(:,2)');
-
 end
 
+% compute position
 pos = (dxp .* dxe + dyp .* dye) ./ (dxe .* dxe + dye .* dye);
