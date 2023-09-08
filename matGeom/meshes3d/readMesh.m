7c2e1b760e3943c499c5426be8e6a0cd8a07ad09
oqilipo <>
2023-09-08 18:32:56 +0200

    Readded trimMesh function to readMesh.m but set to false as default.

--- matGeom/meshes3d/readMesh.m
+++ matGeom/meshes3d/readMesh.m
@@ -1,15 +1,19 @@
 function varargout = readMesh(fileName, varargin)
 %READMESH Read a 3D mesh by inferring format from file name.
 %
-%   Usage:
 %   [V, F] = readMesh(FILENAME)
 %   Read the data stored in file FILENAME and return the vertex and face
 %   arrays as NV-by-3 array and NF-by-N array respectively, where NV is the
 %   number of vertices and NF is the number of faces.
 %
 %   MESH = readMesh(FILENAME)
-%   Read the data stored in file FILENAME and return the mesh into a struct
+%   Read the data stored in file FILENAME and return the mesh as a struct
 %   with fields 'vertices' and 'faces'.
+%
+%   [V, F] = readMesh(FILENAME, 'trimMesh', true)
+%   By default the mesh is not altered during reading. However, the memory 
+%   footprint of the mesh can be reduced by setting the 'trimMesh' option 
+%   to 'true'. See the trimMesh function for further information.
 %
 %   Example
 %     mesh = readMesh('apple.ply');
@@ -27,7 +31,8 @@ function varargout = readMesh(fileName, varargin)
 % Copyright 2020-2023 INRAE - BIA Research Unit - BIBS Platform (Nantes)
 
 parser = inputParser;
-addParameter(parser, 'trimMesh', true, @islogical);
+logParValidFunc = @(x) (islogical(x) || isequal(x,1) || isequal(x,0));
+addParameter(parser, 'trimMesh', false, logParValidFunc);
 parse(parser, varargin{:});
 
 [~, ~, ext] = fileparts(fileName);
@@ -42,6 +47,10 @@ switch lower(ext)
         mesh = readMesh_obj(fileName);
     otherwise
         error('readMesh.m function does not support %s files.', upper(ext(2:end)));
+end
+
+if parser.Results.trimMesh
+    mesh = trimMesh(mesh);
 end
 
 % format output arguments
