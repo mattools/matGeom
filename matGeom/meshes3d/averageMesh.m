4a7b48beef63698e3e49e1d23461bb51e34619c7
dlegland <david.legland@inrae.fr>
2023-11-17 16:49:19 +0100

    meshes3d/averageMesh.m: update documentation

--- matGeom/meshes3d/averageMesh.m
+++ matGeom/meshes3d/averageMesh.m
@@ -1,13 +1,37 @@
-function [refMesh, distListIters, refVerticesIters] = averageMesh(meshList, varargin)
+function [avgMesh, distsIters, verticesIters] = averageMesh(meshList, varargin)
 %AVERAGEMESH Compute average mesh from a list of meshes.
 %
-%   AVG = averageMesh(MESHLIST)
+%   AVGMESH = averageMesh(MESHLIST)
+%   Where MESHLIST is a cell array of meshes, computes an average mesh that
+%   minimizes the sum of squared distances between average mesh vertices
+%   and all other mesh vertices.
+%   The method is to choose an arbitrary reference mesh, and to iterate a
+%   series of smoothin of the reference mesh, computation of nearest vertex
+%   neighbors, and computing average position of nearest neighbors.
+%
+%   [AVGMESH, DISTS] = averageMesh(MESHLIST)
+%   Returns also a cell array containing for each iteration, the standard
+%   deviation of distances to other individual meshes.
+%
+%   [AVGMESH, DISTS, VERT_ITERS] = averageMesh(MESHLIST)
+%   Also returns for each iteration, the positions of the average vertices.
+%
+%   [AVGMESH, DISTS] = averageMesh(..., PNAME, PVALUE)
+%   Provides addition options are parameter name-value pairs. Available
+%   options are:
+%   * verbose: (logical, default false) display or not information about
+%       process
+%   * nIters: number of smooth-projection iterations to perform. Default
+%       value is 10.
+%   * smoothingIteration: the number of smoothing operations to apply on
+%       average mesh at each iteration. Default value is 3.
+%
 %
 %   Example
 %   averageMesh
 %
 %   See also 
-%     meshes3d
+%     meshes3d, smoothMesh
 
 % ------
 % Author: David Legland
@@ -20,6 +44,7 @@ function [refMesh, distListIters, refVerticesIters] = averageMesh(meshList, vara
 % default values
 nIters = 10;
 verbose = false;
+smoothingIterations = 3;
 
 % parse input arguments
 while length(varargin) > 1
@@ -32,6 +57,8 @@ while length(varargin) > 1
         verbose = varargin{2};
     elseif strcmpi(name, 'nIters')
         nIters = varargin{2};
+    elseif strcmpi(name, 'smoothingIterations')
+        smoothingIterations = varargin{2};
     else
         error(['Unknown parameter name: ' name]);
     end
@@ -50,23 +77,25 @@ for iMesh = 1:nMeshes
 end
 
 % choose arbitrary initial mesh
-refMesh = struct('vertices', meshList{1}.vertices, 'faces', meshList{1}.faces);
+avgMesh = struct('vertices', meshList{1}.vertices, 'faces', meshList{1}.faces);
 
-refVerticesIters = cell(1, nIters);
-distListIters = cell(1, nIters);
+verticesIters = cell(1, nIters);
+distsIters = cell(1, nIters);
 
 
 %% Main iteration
 
+% iterates smoothing + computation of average projections
 for iIter = 1:nIters
     if verbose
         fprintf('iter %d/%d\n', iIter, nIters);
     end
-    refMesh = smoothMesh(refMesh);
+    % apply smoothing to current average mesh
+    avgMesh = smoothMesh(avgMesh, smoothingIterations);
     
     % create new array for average vertices
-    newVerts = zeros(size(refMesh.vertices));
-    distList = zeros(size(refMesh.vertices, 1), 1);
+    newVerts = zeros(size(avgMesh.vertices));
+    dists = zeros(size(avgMesh.vertices, 1), 1);
     
     % iterate over all meshes
     for iMesh = 1:nMeshes
@@ -74,23 +103,26 @@ for iIter = 1:nIters
             fprintf('    mesh %d/%d\n', iMesh, nMeshes);
         end
         
-%         mesh = meshList{iMesh};
-        inds = knnsearch(treeList{iMesh}, refMesh.vertices); 
-
-%         closest = mesh.vertices(inds,:);
+        % for each vertex of reference mesh, find index of closest vertex
+        % in current mesh
+        inds = knnsearch(treeList{iMesh}, avgMesh.vertices); 
+        
+        % keep position of closest vertex to update new position
         closest = treeList{iMesh}.X(inds,:);
         newVerts = newVerts + closest;
-        distList = distList + sum((closest - refMesh.vertices).^2, 2);
+
+        % keep distance to closest index to build variability map
+        dists = dists + sum((closest - avgMesh.vertices).^2, 2);
     end
 
-    % update new vertices
+    % update position of new vertices
     newVerts = newVerts / nMeshes;
-    refVerticesIters{iIter} = newVerts;
-    refMesh.vertices = newVerts;
+    verticesIters{iIter} = newVerts;
+    avgMesh.vertices = newVerts;
     
     % keep list of distances
-    distList = sqrt(distList / nMeshes);
-    distListIters{iIter} = distList;
+    dists = sqrt(dists / nMeshes);
+    distsIters{iIter} = dists;
 end
 
 
