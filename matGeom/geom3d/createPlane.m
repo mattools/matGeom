993c932a6c15933cdf382ca5050040bd01cb02f3
dlegland <david.legland@inrae.fr>
2023-12-19 11:13:28 +0100

    geom3d/createPlane.m: cleanup code and add unit tests

--- matGeom/geom3d/createPlane.m
+++ matGeom/geom3d/createPlane.m
@@ -5,10 +5,10 @@ function plane = createPlane(varargin)
 %   creates a plane containing the 3 points
 %
 %   PLANE = createPlane(PTS) 
-%   The 3 points are packed into a single 3x3 array.
+%   The 3 points are packed into a single 3-by-3 array.
 %
 %   PLANE = createPlane(P0, N);
-%   Creates a plane from a point and from a normal to the plane. The
+%   Creates a plane from a point P0 and a normal N to the plane. The
 %   parameter N is given either as a 3D vector (1-by-3 row vector), or as
 %   [THETA PHI], where THETA is the colatitute (angle with the vertical
 %   axis) and PHI is angle with Ox axis, counted counter-clockwise (both
@@ -38,13 +38,17 @@ function plane = createPlane(varargin)
 % Copyright 2005-2023 INRA - TPV URPOI - BIA IMASTE
 
 if length(varargin) == 1
+    % If a single input is provided, it can be:
+    % * an array of three points belonging to the plane
+    % * a cell array -> one plane per array element is created
+
     var = varargin{1};
-    
     if iscell(var)
         plane = zeros([length(var) 9]);
-        for i=1:length(var)
+        for i = 1:length(var)
             plane(i,:) = createPlane(var{i});
         end
+
     elseif size(var, 1) >= 3
         % 3 points in a single array
         p1 = var(1,:);
@@ -57,10 +61,14 @@ if length(varargin) == 1
 
         % create plane
         plane = normalizePlane([p1 v1 v2]);
-        return;
+
+    else
+        error ('MatGeom:createPlane', 'In case of single a.');
     end
     
 elseif length(varargin) == 2
+    % Two arguments -> correspond to plane origin and plane normal
+
     % plane origin
     p0 = varargin{1};
     
@@ -69,43 +77,45 @@ elseif length(varargin) == 2
     if size(var, 2) == 2
         % normal is given in spherical coordinates
         n = sph2cart2([var ones(size(var, 1))]);
-    elseif size(var, 2)==3
+    elseif size(var, 2) == 3
         % normal is given by a 3D vector
         n = normalizeVector3d(var);
     else
-        error ('wrong number of parameters in createPlane');
+        error ('MatGeom:createPlane', 'Can not parse plane normal.');
     end
     
     % ensure same dimension for parameters
-    if size(p0, 1)==1
+    if size(p0, 1) == 1
         p0 = repmat(p0, [size(n, 1) 1]);
     end
-    if size(n, 1)==1
+    if size(n, 1) == 1
         n = repmat(n, [size(p0, 1) 1]);
     end
 
-    % find a vector not colinear to the normal
-    v0 = repmat([1 0 0], [size(p0, 1) 1]);
-    inds = vectorNorm3d(cross(n, v0, 2))<1e-14;
-    v0(inds, :) = repmat([0 1 0], [sum(inds) 1]);
-%     if abs(cross(n, v0, 2))<1e-14
-%         v0 = repmat([0 1 0], [size(p0, 1) 1]);
-%     end
+    % find a vector not colinear to the normal, in the direction of [1 0 0]
+    % first try with vector [0 0 1]
+    v0 = repmat([0 0 1], [size(p0, 1) 1]);
+    % if vectors are close to colinear, use vector [0 -1 0]
+    inds = vectorNorm3d(cross(n, v0, 2)) < 1e-12;
+    v0(inds, :) = repmat([0 -1 0], [sum(inds) 1]);
     
     % create direction vectors
     v1 = normalizeVector3d(cross(n, v0, 2));
     v2 = -normalizeVector3d(cross(v1, n, 2));
 
-    % concatenate result in the array representing the plane
+    % concatenate results in the array representing the plane
     plane = [p0 v1 v2];
-    return;
     
-elseif length(varargin)==3
+elseif length(varargin) == 3
+    % Three input arguments:
+    % * three points (as 1-by-3 or 1-by-N numeric arrays)
+    % * center, Dip and DipDir (?)
+
     var1 = varargin{1};
     var2 = varargin{2};
     var3 = varargin{3};
-    
     if size(var1, 2) == 3 && size(var2, 2) == 3 && size(var3, 2) == 3
+        % input arguments are three points
         p1 = var1;    
         p2 = var2;
         p3 = var3;
@@ -115,7 +125,7 @@ elseif length(varargin)==3
         v2 = p3 - p1;
 
         plane = normalizePlane([p1 v1 v2]);
-        return;
+        
     elseif size(var1, 2) == 3 && size(var2, 2) == 1 && size(var3, 2) == 1
         p0 = var1;
         n = [sin(var2)*sin(var3) sin(var2)*cos(var3) cos(var2)];
@@ -131,11 +141,11 @@ elseif length(varargin)==3
 
         % concatenate result in the array representing the plane
         plane = [p0 v1 v2];  
-        return;
+        
     else
-        error('Wrong argument in "createPlane".');
+        error('MatGeom:createPlane', 'Wrong argument in "createPlane".');
     end  
 else
-    error('Wrong number of arguments in "createPlane".');
+    error ('MatGeom:createPlane', 'Wrong number of input parameters.');
 end
 
