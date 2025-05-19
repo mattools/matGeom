a904995f5c09da90269b4d5edb336caa63ab7dd0
dlegland <david.legland@inrae.fr>
2025-05-19 16:08:01 +0200

    add options to the installation script

--- matGeom/setupMatGeom.m
+++ matGeom/setupMatGeom.m
@@ -3,10 +3,22 @@ function setupMatGeom(varargin)
 %
 %   Usage:
 %   setupMatGeom;
+%   Run the setup script, adding the paths to the different modules of the
+%   library.
 %
-%   Example
+%   setupMatGeom(PNAME, PVALUE);
+%   Provides optional parameter name-value pairs. Possible parameter names
+%   are:
+%   'verbose'   (logical, default true) display information about the
+%               installation process on the command line
+%   'savePaths' (logical, default false) attempts to save the new 'path'
+%               variable as default for future Matlab startup. This
+%               operation may require administrator priviledge to update
+%               the 'pathdef.m' file. 
+%
+%   Examples
 %   setupMatGeom;
-%
+%   setupMatGeom('verbose', false);
 %
 
 % ------
@@ -14,6 +26,21 @@ function setupMatGeom(varargin)
 % E-mail: david.legland@inrae.fr
 % Created: 2011-01-11, using Matlab 7.9.0.529 (R2009b)
 % Copyright 2011-2024 INRA - Cepia Software Platform
+
+% default option values
+verbose = true;
+savePaths = false;
+
+% parse input arguments 
+parser = inputParser;
+addParameter(parser, 'verbose', verbose, @islogical);
+addParameter(parser, 'savePaths', savePaths, @islogical);
+parser.parse(varargin{:});
+
+% replace default values with options
+verbose = parser.Results.verbose;
+savePaths = parser.Results.savePaths;
+
 
 % extract library path
 fileName = mfilename('fullpath');
@@ -27,14 +54,23 @@ moduleNames = {...
     'meshes3d', ...
     'utils'};
 
-disp('Installing MatGeom Library');
+if verbose
+    disp('Installing MatGeom Library');
+end
 addpath(libDir);
 
 % add all library modules
 for i = 1:length(moduleNames)
     name = moduleNames{i};
-    fprintf('Adding module: %-20s', name);
+    if verbose
+        fprintf('Adding module: %-20s', name);
+    end
     addpath(fullfile(libDir, name));
-    disp(' (ok)');
+    if verbose
+        disp(' (ok)');
+    end
 end
 
+if savePaths
+    savepath;
+end
