2eac149dbb82302357d02cf22e6e4a612b7eeec3
dlegland <david.legland@inrae.fr>
2026-03-03 11:34:35 +0100

    meshes3d/transformMesh.m can now handle transform provided as function handle

--- matGeom/meshes3d/transformMesh.m
+++ matGeom/meshes3d/transformMesh.m
@@ -4,6 +4,12 @@ function varargout = transformMesh(varargin)
 %   MESH2 = transformMesh(MESH1, TRANSFO)
 %   MESH2 = transformMesh(VERTICES, FACES, TRANSFO)
 %   [V2, F2] = transformMesh(...)
+%   Applies the transform TRANSFO to the vertices of the specified mesh.
+%   The mesh can be specified either as a struct with at least the fields
+%   'vertices' and 'faces', or as a pair of vertices-faces arrays.
+%   The transform TRANSFO can be either a 4-by-4 matrix corresponding to an
+%   affine transform, or a function handle accepting an N-by-3 array of
+%   coordinates and returning the array of transformed coordinates.
 %
 %   Example
 %     mesh1 = createOctahedron;
@@ -22,10 +28,10 @@ function varargout = transformMesh(varargin)
 % Copyright 2019-2024 INRA - Cepia Software Platform
 
 % parses input arguments
-[vertices, edges, faces] = parseMeshData(varargin{1:end-1});
+meshData = parseMeshData(varargin{1:end-1});
 transfo = varargin{end};
 
-vertices2 = transformPoint3d(vertices, transfo);
+vertices2 = transformPoint3d(meshData.vertices, transfo);
 
 % format output
-varargout = formatMeshOutput(nargout, vertices2, edges, faces);
+varargout = formatMeshOutput(nargout, vertices2, meshData.faces);
